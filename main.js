17d7d80feabc658c31838004295b37967cfb3951
nikolapalibrk <palibrknikola@gmail.com>
2017-06-20 12:19:59 +0200

    Modifikovan nacin hvatanja win linija

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ var line1 = [],
     line4 = [],
     line5 = [],
     allLines = [],
+    line1Img = [],
+    line2Img = [],
+    line3Img = [],
+    line4Img = [],
+    line5Img = [],
+    allLinesImg = [],
     imgUrl = ['img/ananas.png','img/ananas.png','img/ananas.png','img/ananas.png',
     'img/watermelon.png','img/watermelon.png','img/watermelon.png','img/watermelon.png',
     'img/lemon.png','img/lemon.png','img/lemon.png',
@@ -25,14 +31,14 @@ jQuery(document).ready(function($) {
 function makeColumn(parent) {
         for (var i = 0; i < 3; i++) {
             var rand = Math.floor(Math.random() * imgUrl.length);
-            parent.append('<div class="logo"><img src="' + imgUrl[rand] + '"></div>');
+            parent.append('<div class="logo" data-img="' + imgUrl[rand] + '"><img src="' + imgUrl[rand] + '"></div>');
         }
 }
 //Kreiranje novih elemenata i animacija
 function moveSlot(parent) {
         for (var i = 0; i < 10; i++) {
             var rand = Math.floor(Math.random() * imgUrl.length);
-            parent.prepend('<div class="logo"><img src="' + imgUrl[rand] + '"></div>');
+            parent.prepend('<div class="logo" data-img="' + imgUrl[rand] + '"><img src="' + imgUrl[rand] + '"></div>');
         }
         parent.addClass('animate');
         setTimeout(function  () {
@@ -84,6 +90,15 @@ function checkCombination() {
   $('.roll').removeAttr('disabled').css('backgroundColor', 'tomato');
   var col = document.getElementsByClassName('column');
   for (var i = 0; i < 5; i++) {
+    line1Img.push(col[i].children[0].dataset.img);
+    line2Img.push(col[i].children[1].dataset.img);
+    line3Img.push(col[i].children[2].dataset.img);
+  }
+  line4Img.push(col[0].children[0].dataset.img, col[1].children[1].dataset.img, col[2].children[2].dataset.img, col[3].children[1].dataset.img, col[4].children[0].dataset.img);
+  line5Img.push(col[0].children[2].dataset.img, col[1].children[1].dataset.img, col[2].children[0].dataset.img, col[3].children[1].dataset.img, col[4].children[2].dataset.img);
+  allLinesImg.push(line1Img, line2Img, line3Img, line4Img, line5Img);
+
+  for (var i = 0; i < 5; i++) {
     line1.push(col[i].children[0]);
     line2.push(col[i].children[1]);
     line3.push(col[i].children[2]);
@@ -91,33 +106,49 @@ function checkCombination() {
   line4.push(col[0].children[0], col[1].children[1], col[2].children[2], col[3].children[1], col[4].children[0]);
   line5.push(col[0].children[2], col[1].children[1], col[2].children[0], col[3].children[1], col[4].children[2]);
   allLines.push(line1, line2, line3, line4, line5);
+
   alertWin();
 }
 
-//Provera da li su isti i alert
+Array.prototype.allValuesSame = function() {
+
+    for(var i = 1; i < this.length; i++)
+    {
+        if(this[i] !== this[0])
+            return i;
+    }
+
+    return i - 1;
+}
+
+
+
+//// Provera da li su isti i alert ///
+
 function alertWin () {
-  var winLines = [];
+  var help = [];
+  for (var i = 0; i < allLinesImg.length; i++) {
+    help.push(allLinesImg[i].allValuesSame());
+  }
+
   for (var i = 0; i < allLines.length; i++) {
-    for (var x = 1; x < allLines[i].length; x++) {
-      if(x === 1){
-        if( allLines[i][0].children[0].getAttribute('src') === allLines[i][x].children[0].getAttribute('src') ){
-          allLines[i][0].style.backgroundColor = 'seagreen';
-          allLines[i][x].style.backgroundColor = 'seagreen';
-          allLines[i][0].classList.add('highlight');
-          allLines[i][x].classList.add('highlight');
-          winLines[winLines.length - 1].push(allLines[i][0], allLines[i][x]);
-        }
-      }
-      else if
-        ( allLines[i][0].children[0].getAttribute('src') === allLines[i][x].children[0].getAttribute('src') && allLines[i][x-1].children[0].getAttribute('src') === allLines[i][0].children[0].getAttribute('src') && allLines[i][x-1].style.backgroundColor === 'seagreen'){
-          allLines[i][x].style.backgroundColor = 'seagreen';
-          allLines[i][x].classList.add('highlight');
-          winLines[winLines.length - 1].push(allLines[i][x]);
+    for (var x = 0; x < help[i]; x++) {
+      if (help[i] !== 1) {
+        allLines[i][x].classList.add('highlight');
       }
     }
   }
+  //Brisanje svih array-a
+  help.length = 0;
+
   for (var i = 0; i < allLines.length; i++) {
     allLines[i].length = 0;
   }
+
   allLines.length = 0;
+
+  for (var i = 0; i < allLinesImg.length; i++) {
+    allLinesImg[i].length = 0;
+  }
+  allLinesImg.length = 0;
 }
